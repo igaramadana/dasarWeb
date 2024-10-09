05f67cc3876bf9429f58698f0ea098b05437e574
Iga Ramadana Sahputra <igrmdns085@gmail.com>
2024-10-09 13:46:17 +0700

    JS07-Praktikum 4

---
+++ JS07-Form-Processing/html_aman.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Form Input Aman</title>
+    </head>
+    <body>
+        <h2>Input Aman</h2>
+        <form action="html_aman.php" method="post">
+            <label for="input">Masukkan data :</label><br>
+            <input type="text" id="input" name="input"><br><br>
+            <label for="email">Masukkan Email:</label><br>
+            <input type="email" id="email" name="email"><br><br>
+            <input type="submit" value="Submit">
+        </form>
+
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $input = $_POST['input'];
+            $email = $_POST['email'];
+            if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                echo "<p>Email yang dimasukkan : " . $email . "</p>";
+            } else {
+                echo "<p>Email tidak boleh kosong!</p>";
+            }
+
+            //sanitasi input untuk mencegah XSS
+            $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+
+            //validasi sederhana : pastikan input tidak kosong
+            if (!empty($input)) {
+                echo "<p>Data yang dimasukkan : " . $input . "</p>";
+            } else {
+                echo "<p>Input tidak boleh kosong!</p>";
+            }
+        }
