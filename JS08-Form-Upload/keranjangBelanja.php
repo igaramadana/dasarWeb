9f812ddddfc066fe9ec160eb1c9461351eb6ebb3
Iga Ramadana Sahputra <igrmdns085@gmail.com>
2024-10-16 15:09:18 +0700

    JS08 - Praktikum 7

---
+++ JS08-Form-Upload/keranjangBelanja.php
@@ -0,0 +1,14 @@
+<html>
+    <head></head>
+    <body>
+        <h2>Keranjang Belanja</h2>
+
+        <?php 
+            $beliNovel=$_COOKIE['beliNovel'];
+            $beliBuku=$_COOKIE['beliBuku'];
+
+            echo "jumlah Novel: " . $beliNovel . "<br>";
+            echo "jumlah Buku: " . $beliBuku . "<br>";
+        ?>
+    </body>
+</html>
